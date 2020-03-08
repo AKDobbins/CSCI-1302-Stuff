9f1c653b0e4d2667a8d653503571428ea87ef514
AKDobbins <46545352+AKDobbins@users.noreply.github.com>
2020-03-08 19:24:29 -0400

    Add files via upload

---
+++ HW2/HW2/LinkedList/Node.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Node
+    {
+        //private int _data;
+        private Card _data;
+        private Node _link;
+        
+        public Node(Card data, Node link)
+        {
+            _data = data;
+            _link = link;
+        }
+        //public int Data
+        public Card Data
+        {
+            get { return _data; }
+        }
+        public Node Link
+        {
+            get { return _link; }
+            set { _link = value; }
+        }
+    }
+}
