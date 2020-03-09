2450fba8768a7f50f26c22eb1b7f235e5b2c0bb9
AKDobbins <46545352+AKDobbins@users.noreply.github.com>
2020-03-08 22:08:09 -0400

    Add files via upload

---
+++ HW2/HW2.2/War Card Game/LinkedList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace War_Card_Game
+{
+    class LinkedList
+    {
+        private Node _head;
+        public LinkedList()
+        {
+            _head = null;
+        }
+        public void AddFirst(Card data)
+        {
+            this._head = new Node(data, this._head);
+        }
+        public Card RemoveFirst()
+        {
+            Card data;
+            data = this._head.Data;
+            this._head = this._head.Link;
+            return data;
+        }
+        public void AddLast(Card data)
+        {
+            Node node;
+            node = new Node(data, null);
+
+            Node current = this._head;
+            while (current.Link != null)
+                current = current.Link;
+            current.Link = node;
+        }
+       
+    }
+}
